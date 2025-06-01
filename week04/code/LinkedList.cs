2c7782ee351fe557bfad305d72060a320ea201f9
gregclar2019 <daddygreggy09@gmail.com>
2025-06-01 13:35:55 -0400

    week 4 submission

--- week04/code/LinkedList.cs
+++ week04/code/LinkedList.cs
@@ -2,6 +2,9 @@ using System.Collections;
 
 public class LinkedList : IEnumerable<int>
 {
+    //The ? makes the type nullable — meaning it can either be a Node or null.
+    // private means only this class can see or use this variable.
+    // Other classes or objects can’t access _head directly.
     private Node? _head;
     private Node? _tail;
 
@@ -11,7 +14,20 @@ public class LinkedList : IEnumerable<int>
     public void InsertHead(int value)
     {
         // Create new node
+        // This is a shortcut syntax introduced in C# 9.0 and later.
+        // It’s equivalent to the more traditional version:
+        // Node newNode = new Node(value);
+
         Node newNode = new(value);
+
+        // by the way this can also be done
+        // Node newNode = new Node(); (no value passed) 
+        // newNode.Data = value;  (given the Data property is public)
+
+        // Here's why new(value) is preferred
+        // Encapsulation / Object Integrity
+        // More Maintainable
+
         // If the list is empty, then point both head and tail to the new node.
         if (_head is null)
         {
@@ -21,6 +37,12 @@ public class LinkedList : IEnumerable<int>
         // If the list is not empty, then only head will be affected.
         else
         {
+            // note that In C#, classes are reference types. That means when you do: node1.Next = node2;
+            // You're not copying the whole node2 object — you're just storing a reference (pointer) to it.
+            // That’s perfect for a linked list because:
+            // 1) You want each node to link to the next.
+            // 2) You don’t want to copy data — just refer to the next node.
+
             newNode.Next = _head; // Connect new node to the previous head
             _head.Prev = newNode; // Connect the previous head to the new node
             _head = newNode; // Update the head to point to the new node
@@ -33,6 +55,30 @@ public class LinkedList : IEnumerable<int>
     public void InsertTail(int value)
     {
         // TODO Problem 1
+
+        // getting inspiration from InsertHead
+
+        // create a new node
+        Node newNode = new(value);
+
+        // If the list is empty, then point both head and tail to the new node.
+        if (_tail is null)
+        {
+            _head = newNode;
+            _tail = newNode;
+        }
+            else
+        {
+            // note that In C#, classes are reference types. That means when you do: node1.Next = node2;
+            // You're not copying the whole node2 object — you're just storing a reference (pointer) to it.
+            // That’s perfect for a linked list because:
+            // 1) You want each node to link to the next.
+            // 2) You don’t want to copy data — just refer to the next node.
+
+            newNode.Prev = _tail; // Connect new node to the previous head
+            _tail.Next = newNode; // Connect the previous head to the new node
+            _tail = newNode; // Update the head to point to the new node
+        }
     }
 
 
@@ -51,9 +97,16 @@ public class LinkedList : IEnumerable<int>
         }
         // If the list has more than one item in it, then only the head
         // will be affected.
-        else if (_head is not null)
+        else if (_head is not null) // and assuming that the list has more than 1 item
         {
+            // Note: below can be compact but hard to read
+            // but basically you are accesing the node after the head and then set the valie .prev to null
+            // note that the ! is a null-forgiving operator It tells the compiler: "I know _head.Next is not null, so stop warning me."
+            // so to silence that warning (without adding a null check), the developer adds !
+            //But the line will still crash at runtime if _head.Next is null — for example, if:
             _head.Next!.Prev = null; // Disconnect the second node from the first node
+            // in reference to above you could do Node nextNode = _head.Next; 
+            // and then nextNode.Prev = null;
             _head = _head.Next; // Update the head to point to the second node
         }
     }
@@ -65,6 +118,19 @@ public class LinkedList : IEnumerable<int>
     public void RemoveTail()
     {
         // TODO Problem 2
+
+        // inspired by the prev problem
+        if (_head == _tail)
+        {
+            _head = null;
+            _tail = null;
+        }
+        else if (_tail is not null)
+        {
+            _tail.Prev!.Next = null; // make second to the last node tail and change the next property to null
+            _tail = _tail.Prev;
+        }
+
     }
 
     /// <summary>
@@ -74,6 +140,8 @@ public class LinkedList : IEnumerable<int>
     {
         // Search for the node that matches 'value' by starting at the 
         // head of the list.
+        // It means that curr is a variable of type Node, but it is allowed to be null.
+        // This is called a nullable reference type in C# 8.0 and later.
         Node? curr = _head;
         while (curr is not null)
         {
@@ -109,6 +177,48 @@ public class LinkedList : IEnumerable<int>
     public void Remove(int value)
     {
         // TODO Problem 3
+
+        // as usual start from the head of the list
+        Node? current = _head;
+
+        // Keep looping as long as there's a node to look at.
+        // because if we loop in the tail the current.Next will be null
+        while (current != null)
+        {
+            // when we have found the first node that matches 'value' by starting at the head
+            if (current.Data == value)
+            {
+                // Case 1: Node is the head
+                if (current == _head)
+                {
+                    // _head = current.Next;
+                    // if (_head != null)
+                    //     _head.Prev = null;
+                    // else
+                    //     _tail = null; // list became empty
+                    RemoveHead(); // simply call the RemoveHead method
+                }
+                // Case 2: Node is the tail
+                else if (current == _tail)
+                {
+                    // _tail = current.Prev;
+                    // if (_tail != null)
+                    //     _tail.Next = null;
+                    RemoveTail(); // simply call the RemoveTail method
+                }
+                // Case 3: Node is in the middle
+                else
+                {
+                    // just change the pointer accordingly
+                    current.Prev!.Next = current.Next;
+                    current.Next!.Prev = current.Prev;
+                }
+
+                return; // Exit after removing the first match
+            }
+
+            current = current.Next;
+        }
     }
 
     /// <summary>
@@ -117,12 +227,30 @@ public class LinkedList : IEnumerable<int>
     public void Replace(int oldValue, int newValue)
     {
         // TODO Problem 4
+
+        // as usual start from the head
+        Node? current = _head;
+
+        // while there is still a node
+        // remember if we have reached the tail then the current.next will be null
+        while (current != null)
+        {
+            // when we have found the first Node having the oldValue
+            if (current.Data == oldValue)
+            {
+                // since the Data of each Node is public we can just change the data
+                // and keep the rest of the Node as is
+                current.Data = newValue;
+            }
+
+            current = current.Next;
+        }
     }
 
     /// <summary>
     /// Yields all values in the linked list
     /// </summary>
-    IEnumerator IEnumerable.GetEnumerator()
+    IEnumerator IEnumerable.GetEnumerator() // This line is part of enabling LinkedList to be used in a foreach loop.
     {
         // call the generic version of the method
         return this.GetEnumerator();
@@ -147,7 +275,16 @@ public class LinkedList : IEnumerable<int>
     public IEnumerable Reverse()
     {
         // TODO Problem 5
-        yield return 0; // replace this line with the correct yield return statement(s)
+        //yield return 0; // replace this line with the correct yield return statement(s)
+
+        // this time we are starting from the tail and move back (notice current = current.Prev;)
+        Node? current = _tail;
+
+        while (current != null)
+        {
+            yield return current.Data;
+            current = current.Prev;
+        }
     }
 
     public override string ToString()
--- week04/code/Node.cs
+++ week04/code/Node.cs
@@ -1,10 +1,15 @@
 ﻿// DO NOT MODIFY THIS FILE
 public class Node
 {
+    // defining the properties on this part
+    // public means this property can be accessed or modified from outside the class.
+    // { get; set; } means the value can be read (get) and changed (set).
     public int Data { get; set; }
     public Node? Next { get; set; }
     public Node? Prev { get; set; }
 
+    // this part is the constructor
+    // a special method that's called when you create a new Node.
     public Node(int data)
     {
         this.Data = data;
